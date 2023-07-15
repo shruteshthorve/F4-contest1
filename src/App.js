8bf6fcf5c64e1c03b8b6c4e69e54c7b5ec0e8562
thorveshrutesh <thorveshrutesh@gmail.com>
2023-07-15 23:59:47 +0530

    hii

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Signupform from './Signupform';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>signup form</h1> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Signupform.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+const Signupform = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmpassword] = useState('');
+
+  return (
+    <div>Signupform
+         
+
+    </div>
+    
+  )
+}
+
+export default Signupform
